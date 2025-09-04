8eac99efcca0a4a309e0c807b1bb3d7aa298b1ab
Desai Priyansh <priyanshdesai62@gmail.com>
2025-09-04 17:03:12 +0530

    minor

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,10 +5,10 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  // Fix for Vercel static hosting
+  base: "./", // ✅ Important for 404s & asset paths
   build: {
-    outDir: "build",
+    outDir: "build", // already set
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
